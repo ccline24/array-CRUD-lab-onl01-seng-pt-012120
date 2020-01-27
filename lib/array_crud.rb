a85aaa980489ff8d47c80198138fd348f07792b6
Chance cline <clinechance@gmail.com>
2020-01-27 20:25:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  random=["cats","dogs","tacos","coffee"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  new=[1,2,3]
+  new << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  owen_wilson=["wedding crashers"]
+  owen_wilson.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
+  remove=["old","arrays!"]
+  remove.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  first_is_last=["wow","bye"]
+  first_is_last.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element=["am","not"]
+  retrieve_element[0]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  first_element=["wow","wilson"]
+  first_element[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  last_element=["first","arrays!"]
+  last_element[-1]
 end
