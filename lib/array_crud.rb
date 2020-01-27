d63e6a833678e06f8191b4c265ce53dc70180229
Chance cline <clinechance@gmail.com>
2020-01-27 20:14:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  random["cats","dogs","tacos","coffee"]
+  random=["cats","dogs","tacos","coffee"]
 end
 
 def add_element_to_end_of_array(array, element)
-  new[]
+  new=[]
 end
 
 def add_element_to_start_of_array(array, element)
